ee7e7f611cbb79e870c239f7e7a59eebfedd9480
Kristofer Fangrat <kristofer.fangrat@gmail.com>
2024-09-05 10:53:00 +0200

    changes made

---
+++ exercises_2_dimensional_modeling/IceCream/dbt_code copy/models/src/src_icecream_products.sql
@@ -0,0 +1,20 @@
+{{
+  config(
+    materialized = 'ephemeral',
+    )
+}}
+
+
+WITH stg_data_ice_cream_products AS (SELECT * FROM {{ source('ice_cream_db_exercise', 'ICE_CREAM_PRODUCTS') }})
+
+SELECT
+    id,
+    employer__name AS employer_name,
+    employer__workplace AS employer_workplace,
+    employer__organization_number AS organization_number,
+    workplace_address__street_address AS street_address,
+    workplace_address__region AS workplace_region,
+    workplace_address__postcode AS workplace_postcode,
+    workplace_address__city AS workplace_city,
+    workplace_address__country AS workplace_country
+FROM stg_job_ads
